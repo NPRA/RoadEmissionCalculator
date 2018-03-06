684eeb7600eb38b8dcce1046820fcec783f4926c
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2018-03-06 11:28:06 +0100

    bugfix: QGIS changed the way the handle a QgsRenderer from QgsVectorLayer. Also did some minor python cleanup

--- layer_mng.py
+++ layer_mng.py
@@ -3,9 +3,10 @@ from builtins import range
 from builtins import object
 from qgis.PyQt.QtCore import QVariant
 from qgis.PyQt.QtGui import QColor
-from qgis.core import QgsPointXY
+from qgis.core import QgsPointXY, QgsPoint
 from qgis.core import QgsVectorLayer, QgsField, QgsFeature, QgsGeometry
 from qgis.core import QgsProject
+from qgis.core import QgsRenderContext
 
 
 def enum(**named_values):
@@ -13,29 +14,32 @@ def enum(**named_values):
 
 
 GeometryTypes = enum(POINT="Point", LINE="LineString")
-LayerNames = enum(ROUTE = "Route", SELECTED="Selected_route", STARTPOINT="Start_point", ENDPOINT="End_point")
+LayerNames = enum(ROUTE="Route", SELECTED="Selected_route", STARTPOINT="Start_point", ENDPOINT="End_point")
 
 
 class LayerMng(object):
     '''Layer manager is a class for creating and removing point and polyline layers. Created layers are presented
     in a map and legend.'''
+
     def __init__(self, iface):
         self.iface = iface
 
     def create_layer(self, points, layer_name, type_geometry, style_width, layer_id, color_list):
         # get default CRS
         canvas = self.iface.mapCanvas()
-        # mapRenderer = canvas.mapRenderer()
         map_settings = canvas.mapSettings()
         srs = map_settings.destinationCrs()
+
         # create an empty memory layer
-        if layer_id != None:
+        if layer_id is not None:
             vl = QgsVectorLayer(type_geometry + "?crs=" + srs.authid(), layer_name + str(layer_id + 1), "memory")
         else:
             vl = QgsVectorLayer(type_geometry + "?crs=" + srs.authid(), layer_name, "memory")
+
         # define and add a field ID to memory layer "Route"
         provider = vl.dataProvider()
         provider.addAttributes([QgsField("ID", QVariant.Int)])
+
         # create a new feature for the layer "Route"
         ft = QgsFeature()
         # set the value 1 to the new field "ID"
@@ -45,7 +49,7 @@ class LayerMng(object):
 
             for i in range(len(points)):
                 if (i + 1) < len(points):
-                    line_points.append(QgsPointXY(points[i][0], points[i][1]))
+                    line_points.append(QgsPoint(points[i][0], points[i][1]))
             # set the geometry defined from the point
             ft.setGeometry(QgsGeometry.fromPolyline(line_points))
         else:
@@ -55,13 +59,16 @@ class LayerMng(object):
 
         if type_geometry == GeometryTypes.LINE:
             # set color
-            symbols = vl.rendererV2().symbols()
+            # symbols = vl.rendererV2().symbols()
+            render_ctx = QgsRenderContext()
+            renderer = vl.renderer()
+            symbols = renderer.symbols(render_ctx)
             sym = symbols[0]
-            if layer_id != None and color_list != None:
+
+            if layer_id and color_list:
                 color = color_list[layer_id]
                 sym.setColor(QColor.fromRgb(color[0], color[1], color[2]))
 
-            # set width
             sym.setWidth(style_width)
 
         # add layer to the registry and over the map canvas
