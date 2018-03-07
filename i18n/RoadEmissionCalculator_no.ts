3a082b96ac163cb6e955612b77bf2be9beee429f
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2018-03-07 14:00:58 +0100

    added norwegian translation - first attempt

---
+++ i18n/RoadEmissionCalculator_no.ts
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="no" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>Good morning</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="35"/>
+        <source>Road Emission Calculator</source>
+        <translation>Utslippskalkulator</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="69"/>
+        <source>Summary</source>
+        <translation>Sammendrag</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="185"/>
+        <source>Show results in graph</source>
+        <translation>Vis resultater i graf</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="219"/>
+        <source>Cumulative curve</source>
+        <translation>Kumulativ kurve</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="238"/>
+        <source>Show results in graph</source>
+        <translation>Vis resultater i graf</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="306"/>
+        <source>Pollutants:</source>
+        <translation>Utslipp:</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="384"/>
+        <source>Route parameters</source>
+        <translation>Rute parametere</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="442"/>
+        <source>Start point:</source>
+        <translation>Startpunkt:</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="569"/>
+        <source>End point:</source>
+        <translation>Sluttpunkt:</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="768"/>
+        <source>Length:</source>
+        <translation>Lengde:</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="818"/>
+        <source>Load:</source>
+        <translation>Last:</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="838"/>
+        <source>Set vehicle type</source>
+        <translation>Velg type kjøretøy</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="892"/>
+        <source>Euro Standard:</source>
+        <translation>Euro Standard:</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="911"/>
+        <source>Mode:</source>
+        <translation>Modus:</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="1033"/>
+        <source>Vehicle type:</source>
+        <translation>Type kjøretøy:</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="1077"/>
+        <source>Fuel type:</source>
+        <translation>Drivstoff:</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="1113"/>
+        <source>Save Settings</source>
+        <translation>Lagre instillinger</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="1135"/>
+        <source>Calculate Emissions</source>
+        <translation>Kalkuler</translation>
+    </message>
+    <message>
+        <location filename="road_emission_calculator_dialog_base.ui" line="1157"/>
+        <source>Load Settings</source>
+        <translation>Last innstillinger</translation>
+    </message>
+</context>
+</TS>
